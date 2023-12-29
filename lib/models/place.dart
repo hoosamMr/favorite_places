220bed9e6c8d9b496f271da6bc220b9d05646e14
hoosamMr <hoosam.mr@gmail.com>
2023-12-29 16:24:17 +0200

    first commit

---
+++ lib/models/place.dart
@@ -0,0 +1,24 @@
+import 'package:uuid/uuid.dart';
+const uuid = Uuid();
+class Plasce{
+  Plasce({required this.title}):id = uuid.v4() ;
+ 
+  final String id;
+  final String title;
+}
+// const double xOrigin = 0;
+// const double yOrigin = 0;
+
+// class Point {
+//   final double x;
+//   final double y;
+
+//   Point(this.x, this.y);
+
+//   // Named constructor
+//   Point.origin()
+//       : x = xOrigin,
+//         y = yOrigin;
+//   Point.home() : x = 5.0,y = 3.0;
+   
+// }
