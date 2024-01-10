f5961c064e0157287cf09954bfd38f0c3f19bb6b
hoosamMr <hoosam.mr@gmail.com>
2024-01-10 17:48:07 +0200

    adding location snapshot

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -2,12 +2,29 @@ import 'dart:io';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
-class Place{
-  Place({required this.title,required this.image}):id = uuid.v4() ;
- 
+
+class PlaceLocation {
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
+class Place {
+  Place({
+    required this.title,
+    required this.image,
+    //required this.location,
+  }) : id = uuid.v4();
+
   final String id;
   final String title;
   final File image;
+  //final PlaceLocation location;
 }
 // const double xOrigin = 0;
 // const double yOrigin = 0;
--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -7,8 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
-  const AddPlaceScreen({super.key });
-  
+  const AddPlaceScreen({super.key});
+
   @override
   ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreen();
 }
@@ -17,15 +17,17 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
 
-  void _savePlace(){
+  void _savePlace() {
     final enterdTitle = _titleController.text;
-    if(enterdTitle.isEmpty|| _selectedImage == null){
-      
+    if (enterdTitle.isEmpty || _selectedImage == null) {
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlace(enterdTitle, _selectedImage!);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enterdTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -45,19 +47,25 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
           child: Column(
             children: [
               TextField(
-                decoration: const InputDecoration(labelText: 'Title'),
+                decoration: const InputDecoration(
+                  hintText: 'Title',/*const InputDecoration(labelText: 'Title'),*/
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(255, 117, 113, 121),
+                  ),
+                ),
                 controller: _titleController,
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.onBackground,
                 ),
               ),
               const SizedBox(height: 10),
-
-              ImageInput(onPickImage: (image){
-                _selectedImage = image;
-              },),
+              ImageInput(
+                onPickImage: (image) {
+                  _selectedImage = image;
+                },
+              ),
               const SizedBox(height: 10),
-              LocationInput(),
+               LocationInput(),
               const SizedBox(height: 16),
               ElevatedButton.icon(
                 onPressed: _savePlace,
