24bdb8deef5c2d4fcf1466392af0f69bfda929f0
hoosamMr <hoosam.mr@gmail.com>
2024-01-19 21:53:36 +0200

    loading saved places in places list from local DB

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -5,10 +5,38 @@ import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+    return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+  }, version: 1);
+  return db;
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data.map(
+      (row) => Place(
+        id: row['id'] as String,
+        title: row['title'] as String,
+        image: File(row['image'] as String),
+        location: PlaceLocation(
+          latitude: row['lat'] as double,
+          longitude: row['lng'] as double,
+          address: row['address'] as String,
+        ),
+      ),
+    ).toList();
+    state = places;
+  }
 
-  void addPlace(String title, File image,PlaceLocation location) async{
+  void addPlace(String title, File image, PlaceLocation location) async {
     /*path_provider package*/
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     /*path package*/
@@ -16,28 +44,24 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
     final copiedImage = await image.copy('${appDir.path}/$fileName');
 
-    final newPlace = Place(title: title,image: copiedImage, location: location);
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
 
-    final dbPath = await sql.getDatabasesPath();
-    final db = await sql.openDatabase(path.join(dbPath, 'places.db'),
-    onCreate: (db, version){
-      return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
-    },
-      version: 1
-    );
+    final db = await _getDatabase();
     db.insert('user_places', {
-      'id':newPlace.id,
-      'title' : newPlace.title,
-      'image' : newPlace.image.path,
-      'lat' : newPlace.location.latitude,
-      'lng' : newPlace.location.longitude,
-      'address' : newPlace.location.address,
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
     });
 
     state = [newPlace, ...state];
   }
 }
 
-final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
   (ref) => UserPlacesNotifier(),
 );
